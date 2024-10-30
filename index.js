b1ed00f91d74071b3ef68234121a30d8ca7bda70
Harshvardhan295 <harshvardhankhajuria14@gmail.com>
2024-10-30 10:56:17 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let resultEl = document.getElementById("result-el");
 
 function getInputValues() {
-    let num1 = parseFloat(document.getElementById("num1-el").value);
-    let num2 = parseFloat(document.getElementById("num2-el").value);
+    let num1 = parseInt(document.getElementById("num1-el").value);
+    let num2 = parseInt(document.getElementById("num2-el").value);
     return { num1, num2 };
 }
 
@@ -15,6 +15,8 @@ function add() {
 function subtract() {
     let { num1, num2 } = getInputValues();
     let result = num1 - num2;
+    if (num2>num2){
+    resultEl.textContent = "Difference: -" + result;}
     resultEl.textContent = "Difference: " + result;
 }
 
